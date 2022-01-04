6c9338d2c4cfc2fbe02981d3afae9bc04e42231d
clevinjuna <alinebiche@gmail.com>
2022-01-04 16:21:29 +0100

    ok

--- migrations/Version20220104093534.php
+++ migrations/Version20220104093534.php
@@ -10,22 +10,22 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220104135731 extends AbstractMigration
+final class Version20220104093534 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Ajout d\'une colonne slug à ma table article ';
+        return '';
     }
 
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE article ADD slug VARCHAR(255) NOT NULL');
+        $this->addSql('CREATE TABLE article (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, content LONGTEXT DEFAULT NULL, published_at DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE article DROP slug');
+        $this->addSql('DROP TABLE article');
     }
 }
